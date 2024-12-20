9bf36b501b64771b7914a2eda39962b7bf08f2ca
Stephanie Preuthen <kann.zaubern@gmail.com>
2024-12-20 12:29:30 +0100

    add toast message solution

--- js-create-element/js-createelement_toast-messages/js/index.js
+++ js-create-element/js-createelement_toast-messages/js/index.js
@@ -5,9 +5,16 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
+  const toastMessage = document.createElement("li");
+  toastMessage.classList.add("toast-container__message");
+  toastMessage.textContent = "I'm another toast";
+
+  toastContainer.append(toastMessage);
+
   // Exercise: Append a new entry to the toast messages container
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
