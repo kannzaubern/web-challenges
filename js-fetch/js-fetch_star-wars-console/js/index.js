ab01f657021638bca561514cad3106797ce06a64
Stephanie Preuthen <kann.zaubern@gmail.com>
2025-01-08 15:51:38 +0100

    challenge solution

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,18 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log("Data Object: ", data);
+  console.log(
+    "The hair color of",
+    data.results[0].name,
+    "is",
+    data.results[0].hair_color
+  );
+  console.log("Eye Color of R2D2: ", data.results[2].eye_color);
+}
 
 fetchData();
