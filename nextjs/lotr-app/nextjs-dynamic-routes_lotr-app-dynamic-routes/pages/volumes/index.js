c715027820aee471c123c0ff97e80c9332000fd9
Stephanie Preuthen <kann.zaubern@gmail.com>
2025-01-27 15:33:28 +0100

    add challenge solution

--- nextjs/lotr-app/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
+++ nextjs/lotr-app/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -1,26 +1,28 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { introduction } from "../../lib/data";
+import { volumes } from "../../lib/data";
 
 export default function Volumes() {
+  const router = useRouter();
+
+  const goToRandomVolume = () => {
+    const randomVolume = volumes[Math.floor(Math.random() * volumes.length)];
+    router.push(`/volumes/${randomVolume.slug}`);
+  };
+
   return (
     <>
       <h1>The Lord of the Rings</h1>
       <p>{introduction}</p>
       <ul>
-        <li>
-          <Link href="/volumes/the-fellowship-of-the-ring">
-            The Fellowship of the Ring
-          </Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-two-towers">The Two Towers</Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-return-of-the-king">
-            The Return of the King
-          </Link>
-        </li>
+        {volumes.map(({ slug, title }) => (
+          <li key={slug}>
+            <Link href={`/volumes/${slug}`}>{title}</Link>
+          </li>
+        ))}
       </ul>
+      <button onClick={goToRandomVolume}>Go to Random Volume</button>
     </>
   );
 }
